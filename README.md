155fa84f9a2aab4d61ec08a5737d85877dae89a0
Sobassy <altoscience1110@gmail.com>
2023-07-30 13:52:31 +0900

    add: v1.1 (splash, favicon, and title)

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # BluebirdReplacer
 
 Chrome 版 Twitter のロゴを従来の青い鳥に置換する Chrome 拡張です
+
+## Versions
+
+### 1.0
+
+- メイン画面の左上のロゴを置換
+
+### 1.1
+
+- Splash 画面のロゴを置換
+- favicon を置換
+- ページタイトルを置換
--- editor.js
+++ editor.js
@@ -23,26 +23,20 @@ const oldLogo = `
   </svg>
 `;
 
-window.addEventListener("load", function () {
-  var state = localStorage.getItem("enableBluebird");
-  if (!state) {
-    state = "off";
-    this.localStorage.setItem("enableBluebird", "off");
-  }
+const oldSplashLogo = `
+  <svg
+    viewBox="0 0 24 24"
+    aria-hidden="true"
+    class="r-16ek5rh r-4qtqp9 r-yyyyoo r-16y2uox r-8kz0gk r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-lrsllp"
+    style="color: rgba(29,155,240,1.00);width: 72px;height: 72pxmargin-bottom: auto;margin-left: auto;margin-right: auto;margin-top: auto;top: 0px;bottom: 0px;right: 0px;left: 0px"
+  >
+    <g>
+      <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
+    </g>
+  </svg>
+`;
 
-  if (state === "on") {
-    const jsInitCheckTimer = setInterval(pageLoaded, 1);
-    function pageLoaded() {
-      var logoDOM = document.getElementsByXPath("//h1/a/div");
-      if (logoDOM.length > 0) {
-        logoDOM[0].innerHTML = oldLogo;
-        clearInterval(jsInitCheckTimer);
-      }
-    }
-  }
-});
-
-document.getElementsByXPath = function (expression, parentElement) {
+function getElementsByXPath(expression, parentElement) {
   var r = [];
   var x = document.evaluate(
     expression,
@@ -55,7 +49,81 @@ document.getElementsByXPath = function (expression, parentElement) {
     r.push(x.snapshotItem(i));
   }
   return r;
-};
+}
+
+// favicon window logo and title
+function faviconTitleReplace() {
+  var state = localStorage.getItem("enableBluebird");
+  // favicon
+  const linkDOM = document.getElementsByTagName("link");
+  for (let i = 0; i < linkDOM.length; i++) {
+    const element = linkDOM[i];
+    if (element.rel === "shortcut icon") {
+      if (state === "on") {
+        element.href = element.href.replace("3", "2");
+      } else {
+        element.href = element.href.replace("2", "3");
+      }
+    }
+  }
+
+  // title (og)
+  const metaDOM = document.getElementsByTagName("meta");
+  for (let i = 0; i < metaDOM.length; i++) {
+    const element = metaDOM[i];
+    if (
+      element.content.indexOf("/ X") !== -1 ||
+      element.content.indexOf("/ Twitter") !== -1
+    ) {
+      if (state === "on") {
+        element.content = element.content.replace("X", "Twitter");
+      } else {
+        element.content = element.content.replace("Twitter", "X");
+      }
+    }
+  }
+
+  // title (main)
+  const titleDOM = document.getElementsByTagName("title");
+  if (titleDOM.length > 0) {
+    if (state === "on" && titleDOM[0].innerHTML.indexOf("X") !== -1) {
+      titleDOM[0].innerHTML = titleDOM[0].innerHTML.replace("X", "Twitter");
+    } else if (
+      state === "off" &&
+      titleDOM[0].innerHTML.indexOf("Twitter") !== -1
+    ) {
+      titleDOM[0].innerHTML = titleDOM[0].innerHTML.replace("Twitter", "X");
+    }
+  }
+}
+
+var state = localStorage.getItem("enableBluebird");
+if (!state) {
+  state = "off";
+  localStorage.setItem("enableBluebird", "off");
+} else if (state === "on") {
+  const mainLogoInterval = setInterval(mainLogoReplace);
+  const splashLogoInterval = setInterval(splashLogoReplace);
+  setInterval(faviconTitleReplace);
+
+  // main window logo
+  function mainLogoReplace() {
+    const logoDOM = getElementsByXPath("//h1/a/div");
+    if (logoDOM.length > 0) {
+      logoDOM[0].innerHTML = oldLogo;
+      clearInterval(mainLogoInterval);
+    }
+  }
+
+  // splash window logo
+  function splashLogoReplace() {
+    const logoDOM = getElementsByXPath('//div[@aria-label="Loading…"]');
+    if (logoDOM.length > 0) {
+      logoDOM[0].innerHTML = oldSplashLogo;
+      clearInterval(splashLogoInterval);
+    }
+  }
+}
 
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
   if (msg.cmd === "check") {
@@ -68,7 +136,7 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     sendResponse(state);
   } else if (msg.cmd == "replace") {
     // run replace
-    var logoDOM = document.getElementsByXPath("//h1/a/div");
+    var logoDOM = getElementsByXPath("//h1/a/div");
     if (logoDOM.length === 0) {
       sendResponse(undefined);
     } else {
